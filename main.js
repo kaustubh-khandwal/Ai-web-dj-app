b7e013b67ca0c3430f69cb04d0b1ac89ac43af30
kaustubh-khandwal <76221706+kaustubh-khandwal@users.noreply.github.com>
2022-04-15 20:18:26 +0530

    part2

--- main.js
+++ main.js
@@ -1,4 +1,23 @@
+L_theme = "";
+Light_them = "";
+function preload() {
+    L_theme = loadSound("L theme.mp3");
+    Light_theme = loadSound("Light theme.mp3");
+}
+
 function setup(){
     canvas = createCanvas(300,300);
     canvas.position(500,200)
+
+    video = createCapture(VIDEO)
+    video.hide()
+}
+
+function draw() {
+    image(video,0,0,600,500)
+}
+
+function play() {
+    L_theme.play()
+    Light_theme.play()
 }
