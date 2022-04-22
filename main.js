2998d39f0833c1eeffa403799dd5a5947ddccd6e
kaustubh-khandwal <76221706+kaustubh-khandwal@users.noreply.github.com>
2022-04-22 16:52:55 +0530

    part 4

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 L_theme = "";
 Light_them = "";
+song_status = "";
 leftwristX= 0;
 leftwristY = 0;
 rightwristX = 0;
@@ -28,6 +29,21 @@ function modelloaded() {
 
 function draw() {
     image(video,0,0,600,500)
+
+    
+    fill("#FF0000")
+    stroke("#8B0000")
+    
+    song_status = L_theme.isPlaying();
+    if (scoreleftwrist > 0.2) {
+        circle(leftwristX,leftwristY,20)
+        Light_theme.stop()
+    }
+
+    if (song_status == false) {
+        L_theme.play()
+        document.getElementById("song_name").innerHTML = "L's theme";
+    }
 }
 
 function play() {
@@ -38,6 +54,9 @@ function play() {
 function gotPoses(results) {
     if (results.length > 0) {
         console.log(results)
+        scoreleftwrist = results[0].keypoints[9].score;
+        console.log("scoreleftwrist = " + scoreleftwrist)
+
         leftwristX = results[0].pose.leftWrist.x;
         leftwristY = results[0].pose.leftWrist.y;
         console.log("leftwristX = " + leftwristX + "leftwristY = " + leftwristY)
